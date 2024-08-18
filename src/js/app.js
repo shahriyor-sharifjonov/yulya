5819e4d6ffd342e63af17d307d66988b6ff83d66
shahriyor-sharifjonov <shahriyordeveloping@gmail.com>
2024-08-18 14:48:59 +0500

    init

---
+++ src/js/app.js
@@ -0,0 +1,32 @@
+import * as functions from "./modules/functions.js";
+
+functions.isWebp();
+
+const headerButton = document.querySelector(".header__button");
+const headerMenu = document.querySelector(".header__menu");
+const headerLinks = document.querySelectorAll(".header__link");
+
+let menuOpened = false;
+const menuToggle = () => {
+  menuOpened = !menuOpened;
+  headerButton.classList.toggle("open");
+  headerMenu.classList.toggle("open");
+};
+
+headerButton.onclick = menuToggle;
+
+
+window.onclick = (e) => {
+  if (
+    menuOpened &&
+    !e.composedPath().includes(headerButton) &&
+    !e.composedPath().includes(headerMenu)
+  )
+    menuToggle();
+};
+
+headerLinks.forEach(el => {
+  el.addEventListener('click', () => {
+    menuToggle()
+  })
+})
